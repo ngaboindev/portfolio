0985133eec65940c66cc2047c30568d17bbb4e34
ngaboindev <robertngabo2002@gmail.com>
2022-06-06 16:49:57 +0200

    feat: added nprogress for page transition

--- pages/_app.js
+++ pages/_app.js
@@ -1,10 +1,12 @@
 import '@/styles/globals.css';
 import { ThemeProvider } from 'next-themes';
+import NextNProgress from 'nextjs-progressbar';
 
 function MyApp({ Component, pageProps }) {
   return (
     <ThemeProvider attribute="class" enableSystem={false} defaultTheme="dark">
-      <Component {...pageProps} />
+      <NextNProgress />
+      <Component {...pageProps} />;
     </ThemeProvider>
   );
 }
