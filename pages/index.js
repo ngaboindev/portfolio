9a3d995961337b9ed19f951494c38310bc024e46
ngaboindev <robertngabo2002@gmail.com>
2022-07-13 12:44:36 +0200

    feat: redesigned homepage

--- pages/index.js
+++ pages/index.js
@@ -6,48 +6,46 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <Layout>
-      <div className="flex flex-col-reverse sm:flex-row items-center mt-2 mb-24">
+      <div className="flex flex-col-reverse gap-3 lg:flex-row  items-center mt-2 mb-24">
         <div className="flex flex-col pr-10  text-center md:text-left">
-          <h1 className="text-xl text-gray-700 tracking-tight mb-1 dark:text-gray-400 md:text-3xl font-black">
-            Robert Ngabo
+          <h1 className="text-3xl  text-gray-700 tracking-tight mb-1 dark:text-gray-100 md:text-4xl font-semibold">
+            Hi, I'm Robert Ngabo.
+            <span className="block text-2xl mt-2 font-medium">
+              I enjoy manipulating data and hacking things up
+            </span>
           </h1>
-          <p className="text-gray-600 dark:text-gray-300 my-5">
-            a software developer with a passion for designing and building web
-            apps.
-          </p>
+          <div className="text-gray-600 mb-4 dark:text-gray-200">
+            <p className="my-5">
+              I'm a software developer who enjoys designing and building web
+              apps - mainly using Next,React, Node, and Typescript.
+            </p>
+            <p className="leading-7">
+              My favorite part about coding is the constant innovation of
+              architectures, libraries, and frameworks. I think web development
+              is experiencing a major breakthrough with headless architecture
+              and jamstack, and want to be involved in any way I can.
+            </p>
+          </div>
+
           <p className="text-gray-600 dark:text-gray-300">
             Contact me via{' '}
-            <a href="mailto:robbingabo9@gmail.com" className="text-blue-500">
+            <a
+              href="mailto:robbingabo9@gmail.com"
+              className="text-blue-500 dark:text-blue-400"
+            >
               Email
             </a>{' '}
           </p>
         </div>
-        <div className="w-[80px] sm:w-[176px] flex-shrink-0 relative mb-8 sm:mb-0 text-center lg:mr-auto">
+        <div className="flex-shrink-0 relative mb-8 sm:mb-0 text-center lg:mr-auto">
           <Image
-            src="/images/profile.jpg"
-            className="rounded-full"
-            height={344}
-            width={344}
+            src="/images/profile.png"
+            className="rounded-lg"
+            height={500}
+            width={500}
             priority
             alt="Robert Ngabo"
           />
-        </div>
-      </div>
-      <div className="text-gray-600 dark:text-gray-300 mb-8">
-        <h3 className="text-xl border-b border-gray-100 dark:border-gray-800 pb-2">
-          About Me
-        </h3>
-        <div className="my-5">
-          <p className="mb-8">
-            I love building software and hacking things up , I am currently
-            building with React,Nextjs,Vuejs,Typescript,Tailwind and Nodejs.
-          </p>
-          <p>
-            My favorite part about coding is the constant innovation of
-            architectures, libraries, and frameworks. I think web development is
-            experiencing a major breakthrough with headless architecture and
-            jamstack, and want to be involved in any way I can.
-          </p>
         </div>
       </div>
     </Layout>
