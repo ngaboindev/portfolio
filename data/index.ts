88f8588604414425566efd1277d49e913e94737f
Robert Ngabo <robertngabo2002@gmail.com>
2022-08-07 17:16:40 +0200

    feat: added projects page

---
+++ data/index.ts
@@ -0,0 +1,25 @@
+export const PROJECTS = [
+  {
+    name: 'Creatingg',
+    url: 'https://creatingg.vercel.app/',
+    tech: [
+      'Next JS',
+      'Prisma',
+      'Cloudinary',
+      'Planetscale',
+      'Chakra UI',
+      'Typescript',
+      'Vercel',
+    ],
+    description:
+      'no-code portfolio website builder, efficiently completed in minutes. It was built to make maintaining a personal portfolio simple. all you need is to just signup and fill in your personal information and get you good to go.',
+  },
+
+  {
+    name: 'Personal website',
+    url: 'https://www.robertngabo.tech/',
+    tech: ['Next JS', 'Typescript', 'TailwindCSS', 'MDX', 'Vercel'],
+    description:
+      "The website you're on right now! it serves as a place for me to experiment with different techs, practice my design skills,write blogs  and more!",
+  },
+];
